6a657bd71c1dcc5369fb6401c7c556f0d5b92880
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2025-03-27 22:35:30 +0900

    print info

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -2610,6 +2610,7 @@ class FluxAttnProcessor2_0:
             
             return hidden_states, encoder_hidden_states
         else:
+            print(f'attention processor hidden_states shape={hidden_states}')
             return hidden_states
 
 
