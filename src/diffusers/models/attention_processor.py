13fbf184a0033fef3fe8e8c20e7b66737e5adb70
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2025-03-28 16:32:32 +0900

    fix bug

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -2620,7 +2620,7 @@ class FluxAttnProcessor2_0:
                     
                     encoder_hidden_states_list = torch.cat(encoder_hidden_states_list, dim=1)
                     hidden_states = torch.cat([encoder_hidden_states_list, hidden_states],dim=1)
-                    hidden_states[:,-4608:-4096,:] = encoder_hidden_states
+                    hidden_states[:,:-4608,:] = encoder_hidden_states[:,:-512,:]
                 else:
                     hidden_states = F.scaled_dot_product_attention(
                         query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
