6818ba00d21418f1703d1d4923f245f6a705dcbb
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2025-03-28 11:01:13 +0900

    remove ctln masks

--- src/diffusers/models/controlnets/controlnet_flux.py
+++ src/diffusers/models/controlnets/controlnet_flux.py
@@ -323,8 +323,6 @@ class FluxControlNetModel(ModelMixin, ConfigMixin, PeftAdapterMixin):
         ids = torch.cat((txt_ids, img_ids), dim=0)
         image_rotary_emb = self.pos_embed(ids)
 
-        print(f'joint_attention_kwargs ={joint_attention_kwargs.keys()}')
-
         block_samples = ()
         for index_block, block in enumerate(self.transformer_blocks):
             if torch.is_grad_enabled() and self.gradient_checkpointing:
@@ -342,7 +340,7 @@ class FluxControlNetModel(ModelMixin, ConfigMixin, PeftAdapterMixin):
                     encoder_hidden_states=encoder_hidden_states,
                     temb=temb,
                     image_rotary_emb=image_rotary_emb,
-                    joint_attention_kwargs=joint_attention_kwargs,
+                    #joint_attention_kwargs=joint_attention_kwargs,
                 )
             block_samples = block_samples + (hidden_states,)
 
@@ -363,7 +361,7 @@ class FluxControlNetModel(ModelMixin, ConfigMixin, PeftAdapterMixin):
                     hidden_states=hidden_states,
                     temb=temb,
                     image_rotary_emb=image_rotary_emb,
-                    joint_attention_kwargs=joint_attention_kwargs,
+                    #joint_attention_kwargs=joint_attention_kwargs,
                 )
             single_block_samples = single_block_samples + (hidden_states[:, encoder_hidden_states.shape[1] :],)
 
